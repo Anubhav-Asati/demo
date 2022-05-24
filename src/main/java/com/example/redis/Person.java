90da9a4d6f22913879fa2f9b75882e0adb31dcdd
unknown <user.email>
2022-05-24 18:46:39 +0530

    first commit

---
+++ src/main/java/com/example/redis/Person.java
@@ -0,0 +1,19 @@
+package com.example.redis;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person implements Serializable {
+
+    private long id;
+    private String name;
+    private int age;
+    private String email;
+
+}
